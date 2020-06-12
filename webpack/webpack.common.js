0f1293258ad870b79ccb8a4bfb657bc6ab5b6343
Chris Ivey <chrisivey01@gmail.com>
2020-06-12 10:06:57 -0500

    ui started

---
+++ webpack/webpack.common.js
@@ -0,0 +1,28 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude:/node_modules/,
+                loader:"babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/transform-runtime"]
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader:"html-loader"
+                    }
+                ],
+            },
+            {
+                test:/\.s[ac]ss$/i,
+                use:["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    devtool: "source-map"
+}
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,16 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+    mode:"development",
+    entry:"./src/index.js",
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(), 
+        new HtmlWebpackPlugin({
+            inject:true,
+            template: "src/index.html"
+        })
+    ]
+})
