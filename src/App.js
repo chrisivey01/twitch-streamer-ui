5fdb6e8629da5ab8ba23c045496a44091637adb8
Chief <chiefthiefyt@gmail.com>
2020-08-18 22:10:24 +0300

    Dark mode ready

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import * as Apis from "./api/api";
 import { useDispatch } from "react-redux";
 import { getTwitchStreamers } from "./store/actions/twitch.actions";
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import TwitchLayout from "./components/twitch-layout";
 import Whitelist from "./components/whitelist";
 import Header from "./components/Header";
+
+import "../src/components/Layout.scss";
+import { makeStyles } from "@material-ui/core";
 
 const App = () => {
     const dispatch = useDispatch();
@@ -17,8 +20,27 @@ const App = () => {
         }, []);
     });
 
+    const [theme, setTheme] = useState("light");
+
+    const toggleTheme = () => {
+        if (document.documentElement.getAttribute("data-theme") == "dark") {
+            document.documentElement.setAttribute("data-theme", "light");
+        } else {
+            document.documentElement.setAttribute("data-theme", "dark");
+        }
+    };
+
+    const useStyle = makeStyles((theme) => ({
+        background: {
+            backgroundColor: "var(--primary)",
+            height: "1350px"
+        }
+    }))
+
+    const classes = useStyle();
+
     return (
-        <div>
+        <div className={classes.background}>
             <Header />
             <Switch>
                 <Route exact path="/" component={TwitchLayout} />
