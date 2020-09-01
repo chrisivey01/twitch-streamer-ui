8539e7a00dc61ad023d501a3aa1d355722b9cbd5
GGChief <chiefthiefyt@gmail.com>
2020-09-01 17:28:49 +0300

    New Sidebar

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import clsx from "clsx";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { Link } from 'react-router-dom';
 import {
     Typography,
@@ -8,8 +10,17 @@ import {
     Toolbar,
     AppBar,
     Switch,
+    Drawer,
+    IconButton,
+    Divider,
+    
 } from "@material-ui/core";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import MenuIcon from "@material-ui/icons/Menu"
 import "./Layout.scss";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     buttons: {
@@ -52,7 +63,61 @@ const useStyles = makeStyles((theme) => ({
         marginTop: "2vh",
         left: "93vw",
         fontSize: "0.8vw",
-    }
+    },
+    root: {
+        display: 'flex',
+      },
+      appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+      },
+      appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+      hide: {
+        display: 'none',
+      },
+      drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+      drawerPaper: {
+        width: drawerWidth,
+      },
+      drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+      },
+      content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+      },
+      contentShift: {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      },
 }));
 
 const toggleTheme = () => {
@@ -65,23 +130,65 @@ const toggleTheme = () => {
 
 const Header = () => {
     const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+  
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };
 
     return (
-        <AppBar className={classes.titleStretch} position="static">
-            <Toolbar>
-                <Typography className={classes.toolBarItems}>PMA-RP</Typography>
-                <ButtonGroup
-                    className={classes.buttonGroup}
-                    aria-label="outlined primary button group"
-                >
-                    <Button component={Link} to="/">Streams</Button>
-                    <Button component={Link} to="/whitelist">Whitelist</Button>
-                </ButtonGroup>
-            </Toolbar>
-            <p className={classes.buttonText}>Light Mode</p>
+        <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar className={classes.titleStretch}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.toolBarItems}>PMA-RP</Typography>
+
+          </Toolbar>
+          <p className={classes.buttonText}>Light Mode</p>
             <Switch className={classes.switch} onChange={toggleTheme}>Toggle Light Theme</Switch>
         </AppBar>
-    );
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <MenuItem className={classes.drawerItems} component={Link} to="/">Home</MenuItem>
+          <MenuItem className={classes.drawerItems} component={Link} to="streams">Streamers</MenuItem>
+          <MenuItem className={classes.drawerItems} component={Link} to="whitelist">Whitelist</MenuItem>
+          <MenuItem className={classes.drawerItems} component={Link} to="discord">Discord</MenuItem>
+          <Divider />
+        </Drawer>
+      </div>
+    )
 };
 
 export default Header;
