7f0f312fac05b2314cd06aafea2ff95c416b034e
chris <chrisivey01@gmail.com>
2020-10-31 17:05:44 -0500

    good stuff

--- src/api/api.js
+++ src/api/api.js
@@ -3,15 +3,14 @@ import axios from "axios";
 let myTwitchUrl;
 let whitelistUrl;
 // if (process.env.NODE_ENV === "development") {
-    myTwitchUrl = "http://localhost:3002/check-if-online/";
-    whitelistUrl = "http://localhost:3003/ip/";
+myTwitchUrl = "http://127.0.0.1:3002/check-if-online/";
+whitelistUrl = "http://127.0.0.1:3003/ip/";
 // } else {
 //     myTwitchUrl = "http://pmarp.com:3002/check-if-online/";
 //     whitelistUrl = "http://pmarp.com:3003/ip/";
 // }
 
-const ipUrl =
-    "https://api.ipify.org?format=json";
+const ipUrl = "https://api.ipify.org?format=json";
 
 export const getTwitchStreamers = () => {
     return axios.post(myTwitchUrl);
@@ -22,5 +21,5 @@ export const getIpUrl = () => {
 };
 
 export const setWhitelist = (ip) => {
-    return axios.post(whitelistUrl, {'ip':ip});
+    return axios.post(whitelistUrl, { ip: ip });
 };
