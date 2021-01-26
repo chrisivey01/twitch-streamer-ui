ee5f7353dd237e4f44bcb215386b99fb2769700c
AvarianKnight <dillskaggs@gmail.com>
2021-01-26 08:14:54 -0600

    feat(recaptcha): add recaptcha to curb any bots

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-let myTwitchUrl;
-let whitelistUrl;
+let myTwitchUrl, whitelistUrl, ipUrl;
 if (process.env.NODE_ENV === "development") {
     myTwitchUrl = "http://localhost:3002/check-if-online/";
-    whitelistUrl = "http://localhost:3003/ip/";
+	whitelistUrl = "http://localhost:3003/whitelist/add-whitelist";
+	ipUrl = "http://localhost:3003/get-ip"
 } else {
-    myTwitchUrl = "http://pmarp.com:3002/check-if-online/";
-    whitelistUrl = "http://pmarp.com:3003/ip/";
+    myTwitchUrl = "http://api.pmarp.com/twitch/fetch-streamers";
+	whitelistUrl = "http://api.pmarp.com/whitelist/add-whitelist";
+	ipUrl = "http://api.pma-rp.com/whitelist/get-ip"
 }
-
-const ipUrl =
-    "https://api.ipify.org?format=json";
 
 export const getTwitchStreamers = () => {
     return axios.post(myTwitchUrl);
@@ -21,6 +19,11 @@ export const getIpUrl = () => {
     return axios.get(ipUrl);
 };
 
-export const setWhitelist = (ip) => {
-    return axios.post(whitelistUrl, {'ip':ip});
+export const setWhitelist = (captcha) => {
+    return axios.post(whitelistUrl, {}, {
+		headers: {
+			'Content-Type': 'application/json',
+			'g-recaptcha-response': captcha
+		}
+	});
 };
--- src/components/Whitelist.js
+++ src/components/Whitelist.js
@@ -1,4 +1,5 @@
 import { Button, Container, Divider, Grid } from "@material-ui/core";
+import Reaptcha from 'reaptcha';
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useEffect } from "react";
 import AdBlockDetect from "react-ad-block-detect";
@@ -25,20 +26,29 @@ const useStyles = makeStyles((theme) => ({
 
 const Whitelist = (props) => {
     const classes = useStyles();
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
+	let captcha = null
+	let verificationReady = false
 
     useEffect(() => {
         Apis.getIpUrl().then((results) => {
-            dispatch(actions.getIp(results.data));
+			dispatch(actions.getIp(results.data));
         });
     }, []);
 
-    const fivem = () => {
-        Apis.setWhitelist(props.ip).then((results) =>
-            console.log(results.data.finishedMessage)
-        );
-        window.location = "fivem://connect/fivem.pmarp.com:30120";
-    };
+	const onLoad = () => {
+		verificationReady = true
+	}
+	
+	const onVerify = (verification) => {
+		Apis.setWhitelist(verification).then((response) => {
+			if (response.data === 'success') {
+				window.location = "fivem://connect/fivem.pmarp.com:30120";
+			} else {
+				alert('Failed to whitelist your ip, please refresh the page and try again.')
+			}
+		})
+	}
 
     return (
         <Container fixed>
@@ -49,21 +59,32 @@ const Whitelist = (props) => {
                 </p>
             </AdBlockDetect>
             <p></p>
-            <Button
-                variant="contained"
-                color="primary"
-                component="span"
-                onClick={() => fivem()}
-            >
-                Launch the game
-            </Button>
+			
             <Grid className={classes.reminder}>
                 This box here is now hidden with a fly over, hover your mouse over the black box to see if it's your IP.
             </Grid>
             <Divider/>
+			<Reaptcha
+				ref={e => (captcha = e)}
+				// actual
+				sitekey="6LeUrSsaAAAAAK0OpPxDixgAFJ_wmLeJCestJGDs"
+				// test
+				// sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+				onVerify={onVerify}
+				onLoad={onLoad}
+				explicit
+			/>
+			<Button
+				disabled={verificationReady}
+                variant="contained"
+                color="primary"
+                component="span"
+				onClick={() => captcha.renderExplicitly().catch()}
+            >
+                Launch the game
+            </Button>
             <p className={classes.text}>
-                {props.ip ? props.ip : null} is now whitelisted until the Linux
-                Box restarts!
+                {props.ip ? props.ip : 'Error: Couldn\'t get ip!'} { props.ip ? 'is now whitelisted until the Linux Box restarts!' : 'Please try turning off your ad-block!'}
             </p>
         </Container>
     );
